620ffd79c40bcc7f18babd2284eefb33a837f96d
areyort4827 <antonioreyesortega03@gmail.com>
2026-03-18 10:41:10 +0100

    Cambiar style a archivo style.css

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if ($usuario) {
         $_SESSION['user'] = $usuario;
-        header("Location: dashboard2.php");
+        header("Location: dashboard.php");
         exit;
     } else {
         $error = "Usuario o contraseña incorrectos.";
