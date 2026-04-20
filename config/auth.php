b81d82105c2341d8eaa7a78ff2215463c4559683
EmersonCruz1 <146641238+EmersonCruz1@users.noreply.github.com>
2026-04-20 18:33:49 +0200

    Mejoras en el sistema de acceso
    Ahora el sistema reconoce correctamente los permisos de administrador y entrenador.

--- config/auth.php
+++ config/auth.php
@@ -9,36 +9,24 @@ require_once 'conexion.php';
 function verificarLogin($email, $password)
 {
     global $pdo;
-
     try {
-        // Buscar usuario por nombre
+      
         $sql = "SELECT id, nombre, email, password, rol, club_id
                 FROM usuarios
                 WHERE email = :email
                 LIMIT 1";
 
         $stmt = $pdo->prepare($sql);
-
-        $stmt->execute([
-            ':email' => $email
-        ]);
-
+        $stmt->execute([':email' => $email]);
         $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    
+       
         if ($usuario && $password === $usuario['password']) {
-
-            // nunca guardar password en sesión
             unset($usuario['password']);
-
-            return $usuario;
+            return $usuario; 
         }
-
         return false;
-
     } catch (PDOException $e) {
         die("Error en login: " . $e->getMessage());
     }
 }
-
-
